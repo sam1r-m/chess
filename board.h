039d29dc2706987c6119edef3262f66a4ed35ed3
Samir <samirmohd246@gmail.com>
2023-11-28 14:11:33 -0500

    Complete initial interfaces for board and square

--- board.h
+++ board.h
@@ -7,12 +7,30 @@
 
 #include <vector>
 #include <string>
+#include <memory>
+
+enum class Color { WHITE, BLACK };
 
 class Board: public Subject {
-    std::vector<std::vector<Square>> Board; // 2D array of Squares
+    std::vector<std::vector<Square>> board; // 2D array of Squares
+    std::vector<std::unique_ptr<Piece>> pieces; // All the pieces on the board (maybe make it shared ptr?)
+
+    public:
+    Board();
+    ~Board();
+
+    void initializeBoard();
+    void resetBoard();
 
 
+    bool isCheck(Color color) const;
+    bool isCheckmate(Color color) const;
+    bool isStalemate(Color color) const; 
 
+    void addPieceAt(Square *square, Color color, Piece piece);
+    void removePieceAt(Square *square);
+
+    bool makeMove(int fromX, int fromY, int toX, int toY);
 
 };
 
--- square.h
+++ square.h
@@ -1,4 +1,23 @@
 #ifndef __SQUARE_H__
 #define __SQUARE_H__
 
+#include <vector>
+#include <iostream>
+
+class Piece;
+
+class Square {
+    const int xPos;
+    const int yPos;
+    Piece *piece; // could use a smart pointer here to share ownership with Piece -> shared_ptr<Piece> piece;
+
+    public:
+    Square(int xPos, int yPos); // (xPos, yPos) -> Square Coordinates
+
+    int getX();
+    int getY();
+};
+
+std::ostream& operator<<(std::ostream &out, Square &square); // For TextDisplay
+
 #endif
