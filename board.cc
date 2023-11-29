bb4ac6462a4cfe07b5d32111aa22201dea55210f
Ian-Yu8 <iyu8614@gmail.com>
2023-11-29 01:14:51 -0500

    Added changeCoords helper

--- board.cc
+++ board.cc
@@ -1,5 +1,12 @@
 #include "board.h"
 
+// changeCoords(x, y) converts given x and y 
+//  coordinates into their corresponding array indices
+void changeCoords(int *x, int *y){
+    *x -= 1;
+    *y = 8 - *y;
+}
+
 Board::Board(){
 
     // create rows of squares with corresponding coordinates
@@ -14,5 +21,25 @@ Board::Board(){
     }
 
     // set up pieces in starting postitions
+
+}
+
+void Board::addPieceAt(int x, int y, Color color, std::unique_ptr<Piece> piece){
+    pieces.emplace_back(piece);
+    changeCoords(&x, &y);
+    
+    
+}
+
+void Board::removePieceAt(int x, int y){
+    changeCoords(&x, &y);
+
+
+}
+
+bool Board::makeMove(int fromX, int fromY, int toX, int toY){
+    changeCoords(&fromX, &fromY);
+    changeCoords(&toX, &toY);
+
 
 }
