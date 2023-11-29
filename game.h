12829c441e1a10cf133bdf785c5d5d12c4482ea0
whoque <whoque@uwaterloo.ca>
2023-11-28 22:31:13 -0500

    Changed game.h and game.cc

--- game.h
+++ game.h
@@ -1,1 +1,29 @@
 // Controller
+#ifndef __GAME_H__
+#define __GAME_H__
+
+#include "board.h"
+#include "piece.h"
+
+#include <vector>
+#include <string>
+#include <memory>
+#include <iostream>
+
+enum class Color { WHITE, BLACK };
+
+class Game {
+private:
+    Board board;
+    bool isWhiteTurn;
+
+public:
+    Game();
+    void startGame();
+    bool makeMove(int fromX, int fromY, int toX, int toY);
+    bool isCheckmate(Color color) const; 
+    bool isStalemate(Color color) const; 
+    void switchTurn();
+};
+
+#endif
