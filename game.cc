5bac5bae8c831132a9c5ed700e9512273c0c9e0c
Ian-Yu8 <iyu8614@gmail.com>
2023-11-29 23:15:16 -0500

    Modified Game

--- game.cc
+++ game.cc
@@ -1,6 +1,7 @@
 #include "game.h"
 
-
+//make helper to convert coordinate string to ints
+//make helper to convert piece string to piece
 
 Game::Game(Board b): board {b} {}
 
@@ -21,7 +22,7 @@ void Game::processCommand(const std::string& command) {
         } else if (cmd == "move") {
             std::string start, end;
             iss >> start >> end;
-            movePiece(start, end);
+            //movePiece(start, end);
 
             if (checkmate) {
                 endGame();
@@ -40,12 +41,12 @@ void Game::processCommand(const std::string& command) {
         if (cmd == "+") {
             std::string pieceType, posn;
             iss >> pieceType >> posn;
-            addPiece(pieceType, posn);
+            //addPiece(pieceType, posn);
 
         } else if (cmd == "-") {
             std::string posn;
             iss >> posn;
-            rmPiece(posn);
+            //rmPiece(posn);
 
         } else if (cmd == "=") {
             std::string color;
@@ -81,6 +82,35 @@ void Game::processCommand(const std::string& command) {
     }
 }
 
+void Game::startGame(std::string wp, std::string bp){
+    bool validPlayer = true;
+
+    //set type of whitePlayer
+    if (wp == "human") whitePlayer = std::make_unique<Human>();
+    else if (wp == "computer[1]") whitePlayer = std::make_unique<Computer1>();
+    else if (wp == "computer[2]") whitePlayer = std::make_unique<Computer2>();
+    else if (wp == "computer[3]") whitePlayer = std::make_unique<Computer3>();
+    else if (wp == "computer[4]") whitePlayer = std::make_unique<Computer4>();
+    else validPlayer = false;
+
+    //set type of blackPlayer
+    if (bp == "human") blackPlayer = std::make_unique<Human>();
+    else if (bp == "computer[1]") blackPlayer = std::make_unique<Computer1>();
+    else if (bp == "computer[2]") blackPlayer = std::make_unique<Computer2>();
+    else if (bp == "computer[3]") blackPlayer = std::make_unique<Computer3>();
+    else if (bp == "computer[4]") whitePlayer = std::make_unique<Computer4>();
+    else validPlayer = false;
+
+    if (validPlayer) gameMode = true;
+    else std::cout << "Invalid player type." << std::endl;
+}
+
+void Game::changeTurn(std::string color){
+    if (color == "black") blackTurn = true;
+    else if (color == "white") blackTurn = false;
+    else std::cout << "Invalid color." << std::endl;
+}
+
 void Game::endGame(){
     if (blackTurn){
         whiteScore++;
@@ -89,22 +119,4 @@ void Game::endGame(){
     }
 
     gameMode = false;
-}
-
-void Game::startGame(std::string wp, std::string bp){
-
-    //set type of whitePlayer
-    if (wp == "human") whitePlayer = std::make_unique<Human>();
-    else if (wp == "computer[1]") whitePlayer = std::make_unique<Computer1>();
-    else if (wp == "computer[2]") whitePlayer = std::make_unique<Computer2>();
-    else if (wp == "computer[3]") whitePlayer = std::make_unique<Computer3>();
-    else if (wp == "computer[4]") whitePlayer = std::make_unique<Computer4>();
-
-    //set type of blackPlayer
-    if (bp == "human") blackPlayer = std::make_unique<Human>();
-    else if (bp == "computer[1]") blackPlayer = std::make_unique<Computer1>();
-    else if (bp == "computer[2]") blackPlayer = std::make_unique<Computer2>();
-    else if (bp == "computer[3]") blackPlayer = std::make_unique<Computer3>();
-    else if (bp == "computer[4]") whitePlayer = std::make_unique<Computer4>();
-
 }
--- game.h
+++ game.h
@@ -22,10 +22,13 @@ class Game {
     std::unique_ptr<Player> blackPlayer;
     int whiteScore, blackScore; //stores white and black's scores
 
+    //make helper to convert coordinate string to ints
+    //make helper to convert piece string to piece
+
     void startGame(std::string wp, std::string bp);
-    void movePiece(std::string start, std::string end); 
-    void addPiece(std::string pieceType, std::string posn);
-    void rmPiece(std::string posn);
+    //void movePiece(std::string start, std::string end); 
+    //void addPiece(std::string pieceType, std::string posn);
+    //void rmPiece(std::string posn);
     void changeTurn(std::string color);
     void endGame();
 
