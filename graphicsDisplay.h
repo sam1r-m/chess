267090f350af14541096a3518ba707473d2d471f
Samir <samirmohd246@gmail.com>
2023-12-04 13:04:33 -0500

    Fix include guards and remove redundant comments

--- graphicsDisplay.h
+++ graphicsDisplay.h
@@ -1,29 +1,5 @@
-// // View
-// #ifndef __GRAPHICSDISPLAY_H__
-// #define __GRAPHICSDISPLAY_H__
-
-// #include <iostream>
-// #include <vector>
-
-// #include "observer.h"
-// #include "window.h"
-
-// class GraphicsDisplay : public Observer {
-//     Xwindow xw;
-//     int boardSize;
-
-// public:
-//    GraphicsDisplay(int width, int height, int boardSize);
-//     void notify(Square &s) override;
-//     void drawBoard();
-//     void drawPiece(Square &square);
-// };
-
-
-// #endif
-
-#ifndef GRAPHICSDISPLAY_H
-#define GRAPHICSDISPLAY_H
+#ifndef __GRAPHICSDISPLAY_H__
+#define __GRAPHICSDISPLAY_H__
 
 #include "window.h"
 #include <vector>
@@ -39,4 +15,4 @@ public:
     void drawBoard();
 };
 
-#endif // GRAPHICSDISPLAY_H
+#endif
--- window.h
+++ window.h
@@ -1,40 +1,6 @@
-// #ifndef __WINDOW_H__
-// #define __WINDOW_H__
-// #include <X11/Xlib.h>
-// #include <iostream>
-// #include <string>
-
-// class Xwindow {
-//   Display *d;
-//   Window w;
-//   int s;
-//   GC gc;
-//   unsigned long colours[5];
-
-// 	// Creates a graphic context gc
-// 	GC create_gc(Display* display, Window win, int reverse_video);
-
-//   public:
-//   Xwindow(int width=500, int height=500);  // Constructor; displays the window.
-//   ~Xwindow();                              // Destructor; destroys the window.
-
-
-// 	enum { White = 0, Black, Red, Green, Blue}; // Available colours.
-
-// 	// Draws a string
-// 	void drawString(int x, int y, std::string msg, int colour = Black);
-
-// 	// Draws a rectangle
-// 	void fillRectangle(int x, int y, int width, int height, int colour=Black);
-
-// 	// Draws a chess piece
-// 	void drawPiece(char piece, int x, int y, int width, int height);
-// };
-
-// #endif
-
 #ifndef __WINDOW_H__
 #define __WINDOW_H__
+
 #include <X11/Xlib.h>
 #include <iostream>
 #include <string>
