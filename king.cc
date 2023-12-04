1ba8cc841e0317e5c6103178b3a61bf4955f98ae
Samir <samirmohd246@gmail.com>
2023-12-04 00:55:32 -0500

    Implement basic functionality of the king
    Need to add logic for checks and castling

--- king.cc
+++ king.cc
@@ -1,15 +1,29 @@
 #include "king.h"
 #include "board.h"
+
 #include <iostream>
+#include <cmath>
 
 King::King(Color col, int posX, int posY): Piece {col, posX, posY}, firstMove {true} {}
 
-King::~King(){
-
-}
+King::~King() {}
 
 bool King::isValidMove(int toX, int toY, Board *board) const{
+    // Ensure the destniation coordinates are within the board
+    if (toX < 1 || toX > 8 || toY < 1 || toY > 8) return false;
 
+    // Check that the move is within one square in any direction
+    if (std::abs(toX - x) > 1 || std::abs(toY - y) > 1) return false;
+
+    // Ensure the destination square is not occupied by a piece of the same color
+    if (board->getSquareAt(toX - 1, 8 - toY).isOccupied() &&
+        board->getSquareAt(toX - 1, 8 - toY).getPiece()->getColor() == this->color) {
+        return false;
+    }
+
+    // >> add code for checks and castling here <<
+
+    return true;
 }
 
 char King::getChar() const{
@@ -19,5 +33,21 @@ char King::getChar() const{
 
 std::vector<Move> King::generateMoves(Board *board) const{
     std::vector<Move> moves;
+
+    // One square in every direction
+    for (int deltaX = -1; deltaX <= 1; ++deltaX) {
+        for (int deltaY = -1; deltaY <= 1; ++deltaY) {
+            if (deltaX == 0 && deltaY == 0) continue; // skip current square
+
+            int toX = x + deltaX;
+            int toY = y + deltaY;
+
+            if (isValidMove(toX, toY, board)) {
+                bool captures = board->getSquareAt(toX - 1, 8 - toY).isOccupied();
+                moves.emplace_back(x, y, toX, toY, captures);
+            }
+        }
+    }
+
     return moves;
 }
--- main.cc
+++ main.cc
@@ -1,11 +1,15 @@
 #include <iostream>
+
 #include "game.h"
+#include "GraphicsDisplay.h"
 
 using namespace std;
 
 int main() {
     Game g;
     string command;
+
+    GraphicsDisplay gDisplay(500, 500, 8); // Initialize a graphics display
 
     while (getline(cin, command)) {
         g.processCommand(command);
